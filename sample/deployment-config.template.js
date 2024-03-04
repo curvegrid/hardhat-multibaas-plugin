63061b9d0f266755c76cfc382ee4fb3cde466ceb
Daniel Briskin <daniel.briskin@gmail.com>
2024-03-04 13:37:15 +0900

    misc clean up to hardhat config, specifically so that npx hardhat compile can run without an mb config

--- sample/deployment-config.template.js
+++ sample/deployment-config.template.js
@@ -1,9 +1,23 @@
 const deploymentConfig = {
-  deploymentEndpoint: "http://localhost:8080",
+  // Private key of the deployer account, beginning with 0x
+  deployerPrivateKey: '0x0000000000000000000000000000000000000000000000000000000000000000',
+
+  // Full URL such as https://abc123.multibaas.com
+  deploymentEndpoint: 'http://localhost:8080',
+
+  // The chain ID of the blockchain network
+  // For example: Curvegrid test network = 2017072401, Ethereum Mainnet = 1, BSC Testnet = 97, BSC Mainnet = 56
   ethChainID: 1337,
-  web3Key: "<API KEY IN WEB3 GROUP>",
-  adminApiKey: "<API KEY IN ADMINISTRATOR GROUP>",
-  deployerPrivateKey: "0x0"
+
+  // API key to access MultiBaas web3 endpoint
+  // Note that the API key MUST be part of the "Web3" group
+  // Create one on MultiBaas via navigation bar > Admin > API Keys
+  web3Key: '<API KEY IN WEB3 GROUP>',
+
+  // API key to access MultiBaas from deployer
+  // Note that the API key MUST be part of the "Administrators" group
+  // Create one on MultiBaas via navigation bar > Admin > API Keys
+  adminApiKey: '<API KEY IN ADMINISTRATOR GROUP>',
 };
 
 module.exports = {
--- sample/hardhat.config.ts
+++ sample/hardhat.config.ts
@@ -1,30 +1,31 @@
-// Copyright (c) 2021 Curvegrid Inc.
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-multibaas-plugin';
+import path from 'path';
 
-import "@nomicfoundation/hardhat-toolbox";
-import { HardhatUserConfig } from "hardhat/config";
-import "hardhat-multibaas-plugin";
-import path from 'node:path';
+let deployerPrivateKey = '0x0000000000000000000000000000000000000000000000000000000000000000';
+let deploymentEndpoint, ethChainID, web3Key, adminApiKey;
 
-// Retrieve and process the config file
-const CONFIG_FILE = path.join(__dirname, `./deployment-config.${process.env.HARDHAT_NETWORK || 'development'}`);
-const {
-  deploymentConfig: {
-    deploymentEndpoint,
-    ethChainID,
-    deployerPrivateKey,
-    web3Key,
-    adminApiKey
-  }
-} = require(CONFIG_FILE);
+if (process.env.HARDHAT_NETWORK) {
+  const CONFIG_FILE = path.join(__dirname, `./deployment-config.${process.env.HARDHAT_NETWORK}`);
+  ({
+    deploymentConfig: { deploymentEndpoint, ethChainID, deployerPrivateKey, web3Key, adminApiKey },
+  } = require(CONFIG_FILE));
+}
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "development",
   networks: {
     development: {
       url: `${deploymentEndpoint}/web3/${web3Key}`,
       chainId: ethChainID,
-      accounts: [ deployerPrivateKey ],
+      accounts: [deployerPrivateKey],
     },
+  },
+  mbConfig: {
+    apiKey: adminApiKey,
+    host: deploymentEndpoint,
+    allowUpdateAddress: ['development'],
+    allowUpdateContract: ['development'],
   },
   paths: {
     sources: "./contracts",
@@ -34,12 +35,6 @@ const config: HardhatUserConfig = {
   },
   mocha: {
     timeout: 20000,
-  },
-  mbConfig: {
-    apiKey: adminApiKey,
-    host: deploymentEndpoint,
-    allowUpdateAddress: ["development"],
-    allowUpdateContract: ["development"],
   },
   solidity: "0.8.13",
 };
